27d8f79d2692bff39e723cf73aa260b36699d083
Daniel Nascimento <dpericles6@gmail.com>
2020-04-14 18:39:46 -0300

    atualizacao github

--- src/app/atacado/lista/lista.component.ts
+++ src/app/atacado/lista/lista.component.ts
@@ -11,7 +11,31 @@ export class ListaComponent implements OnInit {
 
   ngOnInit() {
   }
+  selecionar(n) {
+    var slideIndex;
+    slideIndex = 1;
+    showDivs(slideIndex = n);
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName('btn_invisivel');
+      var dots = document.getElementsByClassName('adicionar');
+      if(n > x.length) {slideIndex = 1}
+      else { (n < x.length)}
+      if(n < 1 ) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].classList.toggle('none');
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace('box_yellow', " ")
+      }
+      x[slideIndex-1].classList.toggle('block');
+      dots[slideIndex-1].classList.toggle('box_yellow');
+    }
 
+  }
+  // selecionar(id) {
+  //   document.getElementById(id).classList.toggle('box_yellow');
+  // }
   downlist: any = 'assets/icones_svg/atacado/arquivo_lista.svg';
   orcamento:any ="assets/icones_svg/atacado/orcamento_list.svg";
   pesquisar: any ='assets/icones_svg/lupa.svg';
