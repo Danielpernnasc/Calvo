d741195015b08d7d8742764ef3be6d6cdd3486ed
Daniel Nascimento <dpericles6@gmail.com>
2020-04-22 18:17:56 -0300

    atualizar page

--- src/app/atacado/hortifruit/hortifruit.module.ts
+++ src/app/atacado/hortifruit/hortifruit.module.ts
@@ -4,13 +4,14 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { HortifruitComponent } from './hortifruit.component';
 import { ModalHortifruitComponent } from './modal-hortifruit/modal-hortifruit.component';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 @NgModule({
   imports: [
     CommonModule,
     AngularSvgIconModule,
+    NgxPaginationModule
   ],
   declarations: [
     HortifruitComponent,
--- src/app/atacado/lista/index.ts
+++ src/app/atacado/lista/index.ts
@@ -1,3 +1,2 @@
 export * from './lista.component';
 export * from './lista.module';
-export * from './lista-routing.module';
